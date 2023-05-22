5ea08657352f34da8d3e561221ded2b8bfc403bd
Matthew Davis <marrthistyping64@gmail.com>
2023-05-22 11:55:13 -0500

    added new features

---
+++ AndroidStudioProjects/MyApplication/Main.kt
@@ -0,0 +1,8 @@
+
+fun main(){
+    var name: String = "John"
+    var age: Int = 5
+    var Height: Double = 175.56
+    println("Hello World")
+    println("My name is ${name}, Im ${age} years old and my height is ${height} cm")
+}
